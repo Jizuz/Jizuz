31b6e092154e5d14cacf7b2c753e7ca5e563f333
Jizuz <qpf123@outlook.com>
2017-12-29 15:32:46 +0800

    增加日志配置

--- Jizuz/src/main/java/com/zshoon/jizuz/config/ShiroConfiguration.java
+++ Jizuz/src/main/java/com/zshoon/jizuz/config/ShiroConfiguration.java
@@ -6,6 +6,8 @@ import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +21,9 @@ import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 
 @Configuration
 public class ShiroConfiguration {
+
+	private static Logger logger = LoggerFactory.getLogger(ShiroConfiguration.class);
+
 	@Bean(name = "shiroFilter")
 	public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager manager) {
 		ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
@@ -49,7 +54,7 @@ public class ShiroConfiguration {
 	// 配置核心安全事务管理器
 	@Bean(name = "securityManager")
 	public SecurityManager securityManager(@Qualifier("authRealm") AuthRealm authRealm) {
-		System.err.println("--------------shiro已经加载----------------");
+		logger.error("--------------shiro已经加载----------------");
 		DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
 		manager.setRealm(authRealm);
 		return manager;
